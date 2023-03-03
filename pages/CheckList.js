577e2feb5c576fdeedbca19d5103b8d26325f89b
daun3 <daun1309@gmail.com>
2023-03-03 13:00:53 +0900

    css:checkList

--- pages/CheckList.js
+++ pages/CheckList.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from '@/styles/checkList.module.scss'
 import Image from 'next/image'
 
 const CheckList = () => {
+
+  const note = useRef();
+
   return (
     <>
       <div className="H1">CHECK LIST</div>
-
-      <section className={styles.note}>
+      {/* <h1 style={{color:"red"}}>sdsad</h1> */}
+      <section ref={note} className={styles.note}>
         <input type="text"/>
         <input type="submit" value="등록"/>
 
         <div className={styles.buttonBox}>
-          <button onClick={()=>{}}></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button onClick={()=>{note.current.style="background:#FFFDC7"}}></button>
+          <button onClick={()=>{note.current.style="background:#FFD1C7"}}></button>
+          <button onClick={()=>{note.current.style="background:#E2FFE5"}}></button>
+          <button onClick={()=>{note.current.style="background:#BAF3FB"}}></button>
+          <button onClick={()=>{note.current.style="background:#D8C1FF"}}></button>
+          <button onClick={()=>{note.current.style="background:#FFFFFF"}}></button>
         </div>
 
       </section>
