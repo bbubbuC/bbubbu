9a026e1712b1c455dcbe6faa54d700b95b9178e7
daun3 <daun1309@gmail.com>
2023-03-08 19:37:50 +0900

    css:청첩장

--- pages/Invitation.js
+++ pages/Invitation.js
@@ -1,35 +1,68 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from '@/styles/invitation.module.scss'
 import Image from 'next/image'
 
 const invitation = () => {
+
+  const frame = useRef();
+  const [inputValue, setValue] = useState('');
+  const [addedValues, setAddedValues] = useState([]);
+
+
+  const changeFrame = (e) => {
+    let newFrame = e.target.src;
+    frame.current.src= newFrame;
+  }
+
+
+  const valueChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const addValue = () => {
+    setAddedValues([...addedValues, inputValue]);
+    setValue('');
+  }
+
+
   return (
     <>
       <div className="H1">청첩장만들기</div>
 
       <section className={styles.mainImg}>
-        <Image src="/img/invitation/s.png" alt='' width={590} height={590}/>
+        
+        <div >
+          <Image className={styles.frame} ref={frame}  alt='' width={593} height={593}/>
+          <Image src="/img/invitation/p.png" alt='' width={593} height={593}/>
+        </div>
       </section>
 
       <section>
         <h4>프레임 선택</h4>
         <div>
-          <Image src="/img/invitation/f01.png" alt='' width={150} height={150}/>
-          <Image src="/img/invitation/f02.png" alt='' width={150} height={150}/>
-          <Image src="/img/invitation/f03.png" alt='' width={150} height={150}/>
-          <Image src="/img/invitation/f04.png" alt='' width={150} height={150}/>
-          <Image src="/img/invitation/f01.png" alt='' width={150} height={150}/>
-          <Image src="/img/invitation/f02.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f01.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f04.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f03.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f04.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f01.png" alt='' width={150} height={150}/>
+          <Image onClick={changeFrame} src="/img/invitation/f04.png" alt='' width={150} height={150}/>
         </div>
       </section>
 
       <section>
         <h4>문구 작성하기</h4>
-        <form>
-          <input type="text" />
-          <input type="submit" />
-        </form>
-        <div className={styles.inputBox}></div>
+        <div>
+          <input onChange={valueChange} value={inputValue} type="text" />
+          <button onClick={addValue}>글 추가</button>
+        </div>
+
+        <div className={styles.inputBox}>
+          <ul>
+            {addedValues.map((value, index) => (
+              <li key={index}>{value}</li>
+            ))}
+          </ul>
+        </div>
       </section>
 
       <section>
--- pages/xx.js
+++ pages/xx.js
@@ -1,77 +1,41 @@
-import React, { useRef, useContext, useEffect, useState } from 'react'
-import { useRouter } from 'next/router';
-import styles from '@/styles/checkList.module.scss'
-import Image from 'next/image'
-import { DbContext } from './component/MyContext';
-
-const CheckList = () => {
-
-  const { todoData, todoDataFun } = useContext(DbContext);
-  const initial = { todo: '' };
-  const [inputValue, setInputValue] = useState(initial);
-  const [editingId, setEditingId] = useState(null);
+import { useState } from 'react';
 
 
-  function dataEdit(id) {
-    const todo = todoData.find(obj => obj.id === id);
-    setInputValue({ todo });
-    setEditingId(id);
-  }
+const xx = () => {
 
-  const edit = (e) => {
-    e.preventDefault();
-    todoDataFun('put', { id: editingId, todo: inputValue.todo });
-    setInputValue(initial);
-    setEditingId(null);
-  }
-
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   return (
-    <>
-      <section className={styles.note}>
-        <form onSubmit={editingId !== null ? edit : create}>
-          <input 
-            type="text" 
-            className={styles.textInput}
-            onChange={valueChange} 
-            value={inputValue.todo}
-            placeholder='결혼식 전 체크리스트를 작성해주세요!' 
-            name="todo" 
-          />
-          <input 
-            type="submit" 
-            value={editingId !== null ? "수정" : "등록"} 
-            className={styles.submitInput}
-          />
-        </form>
+    <div
+      style={{
+        position: 'absolute',
+        left: position.x,
+        top: position.y,
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'red',
+        cursor: 'point',
+      }}
+      onMouseDown={(e) => {
+        const startX = e.pageX - position.x;
+        const startY = e.pageY - position.y;
 
-        <div className={styles.textBox}>
-          {
-            todoData && todoData.map(obj => (
-              <div key={obj.id} className={styles.textList}>
-                <div className={styles.check}>
-                  <input type="checkbox"/>
-                  <p>{obj.todo}</p>
-                </div>
-                <div className={styles.btnBox}>
-                  <button onClick={() => dataEdit(obj.id)}>수정</button>
-                  <button onClick={() => dataDelete(obj.id)}>삭제</button>
-                </div>
-              </div>    
-            ))
-          }
-        </div>
+        const handleMouseMove = (e) => {
+          const newX = e.pageX - startX;
+          const newY = e.pageY - startY;
+          setPosition({ x: newX, y: newY });
+        };
 
-      </section>
+        document.addEventListener('mousemove', handleMouseMove);
 
-    </>
-  )
+        document.addEventListener('mouseup', () => {
+          document.removeEventListener('mousemove', handleMouseMove);
+        });
+      }}
+    />
+  );
+
 }
 
-export default CheckList
+export default xx
 
-
-// 위 코드에서는 editingId를 사용하여 현재 수정 중인 todo의 id를 저장합니다. 
-// 수정 버튼을 누르면 dataEdit 함수가 호출되어 해당 todo의 값을 input에 채웁니다. 
-// 수정 후에는 editingId와 inputValue를 초기화하여 
-// 새로운 todo를 추가할 수 있도록 합니다.
