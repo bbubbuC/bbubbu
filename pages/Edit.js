df4e1b35b576cdc508b9bbe733b4b5850f79417b
leeleejueun <leejueun413@gmail.com>
2023-03-13 10:15:00 +0900

    feat: add quiz

--- pages/Edit.js
+++ pages/Edit.js
@@ -1,14 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from '@/styles/edit.module.scss';
 
 const Edit = () => {
     let mainImg=useRef();
+    let inputWedding=useRef();
+    let [Wed,setWed]=useState(false);
+    let [styleName,setStyleName]=useState(false);
+
     function create(e) { 
         e.preventDefault(e);
     }
     function changeImg(e) {
         var newImg = e.target.src;
         mainImg.current.src=newImg;
+    }
+    function toggle() {
+        setWed(setWed=>!setWed)
+        if(Wed == false){
+            inputWedding.current.disabled = true;
+        }else{
+            inputWedding.current.disabled = false;
+        }
+    }
+    function profile(n){
+        if(n){
+            setStyleName(true) 
+        }else{
+            setStyleName(false) 
+        }
     }
     return(
         <main className={styles.main}>
@@ -22,32 +41,44 @@ const Edit = () => {
                             <input type="text" placeholder="닉네임을 입력해주세요" name="nickname" />
                             <input type="button" value="중복확인" />
                         </div>
-                    </div>
+                    </div> v
                     <div className={styles.gender}>
                         <label for="gender" className={styles.title}>본인선택</label> <br />
                         <div>
                             <input type="radio" id="female" name="gender" value="female" />
-                            <label for="female">신부</label>
-                            <input type="radio" id="male" name="gender" value="male" />
-                            <label for="male">신랑</label>
+                            <label for="female" onClick={()=>profile(1)}>신부</label>
+                            <input type="radio"  id="male" name="gender" value="male" />
+                            <label for="male" onClick={()=>profile(0)}>신랑</label>
                         </div>
                     </div>
                     <div className={styles.weddingday}>
                         <label for="weddingday">예식일</label> <br />
                         <div>
-                            <input type="date" name="weddingday" />
-                            <input type="checkbox" id="yet" />
+                            <input type="date" name="weddingday" ref={inputWedding}/>
+                            <input type="checkbox" id="yet" onClick={()=>toggle()}/>
                             <label for="yet">아직 결정되지 않았어요</label>
                         </div>
                     </div>
                     <div className={styles.profile}>
                         <label for="profile">프로필</label>
-                        <div>
-                            <img ref={mainImg} src="/img/edit/gface01.png" className={styles.profile} />
-                            <img onClick={changeImg} className={styles.face} src="/img/edit/gface01.png" />
-                            <img onClick={changeImg} className={styles.face} src="/img/edit/gface02.png" />
-                            <img onClick={changeImg} className={styles.face} src="/img/edit/gface03.png" />
-                            <img onClick={changeImg} className={styles.face} src="/img/edit/gface04.png" />
+                        <div> 
+                            {
+                                styleName ? 
+                                <>
+                                    <img ref={mainImg} src="/img/edit/gface01.png" className={styles.profile} />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/gface01.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/gface02.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/gface03.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/gface04.png" />
+                                </> : 
+                                <>
+                                    <img ref={mainImg} src="/img/edit/bface01.png" className={styles.profile} />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/bface01.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/bface02.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/bface03.png" />
+                                    <img onClick={changeImg} className={styles.face} src="/img/edit/bface04.png" />
+                                </>
+                            }
                         </div>
                     </div>
                     <div className={styles.submit}>
--- pages/Quiz.js
+++ pages/Quiz.js
@@ -1,222 +1,42 @@
-import React from 'react'
-import styles from '@/styles/quiz.module.css'
+import React, { useRef, useState } from 'react'
+import styles from '@/styles/quiz.module.scss'
 
 const Quiz = () => {
+  const [bg,setBg]=useState(true);
+  const [collect,setCollect]=useState([]);
+  const data = require('public/data/data.json')
+
+  console.log(data[0].title);
+function addData(e) {
+  console.log(e.target.id)
+  setCollect(enter => [...enter, e.target.id])
+
+}
+console.log(collect)
+
   return (
-    <main className={styles.body}>
-    <div className={styles.mars}>
-      <div className={styles.mars_border}></div>
-      <div className={styles.mars_menu}></div>
-      <div className={styles.mars_bg}>
-        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/mars_sunburst.png' />
-          {/* <!-- 빙글빙글 은은한 태양 뒷배경 --> */}
+    <main className={styles.main}>
+      <div className={styles.back}>
+        <div className={bg?`${styles.container}`:`${styles.container} ${styles.noneContainer}`}>
+          <section className={styles.start} >
+            <p>Move to Quiz</p>
+            <p>"Choose a better one"</p>
+            <div className={styles.btn} onClick={() => setBg(false)}>
+              <p>Get Started</p>
+              <img className={styles.rocket} src='/img/quiz/rocket.png' />
+              <img src='/img/quiz/rcloud.png' />
+            </div>
+          </section>
+          <section className={styles.balance}>
+            <h2>{data[quizNumber].title}</h2>
+            <button onClick={}>{data[quizNumber].answera}</button>
+            <button onClick={}>{data[quizNumber].answerb}</button>
+          </section>
+        </div>
       </div>
-      <div className={styles.mars_logo}>
-        <div className={styles.mars_logo__empty}></div>
-        <div className={styles.mars_logo__filled}></div>
-      </div>
-      <input className={styles.radio} checked='checked' id='index1' name='trigger' type='radio' />
-        <input className={styles.radio} id='index2' name='trigger' type='radio' />
-          <input className={styles.radio} id='index3' name='trigger' type='radio' />
-            <input className={styles.radio} id='index4' name='trigger' type='radio' />
-              <input className={styles.radio} id='index5' name='trigger' type='radio' />
-                <input className={styles.radio} id='index6' name='trigger' type='radio' />
-                  <div className={styles.mars_truck}>
-                    <div class='mars_truck__wheel'>
-                      <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/truckwheel.png' />
-                      {/* <!-- 트럭 휠 --> */}
-                    </div>
-                    <div class='mars_truck__wheel--two'>
-                      <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/truckwheel.png' />
-                      {/* <!-- 트럭 휠 --> */}
-                    </div>
-                    <div class='mars_truck__body'>
-                      <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/truckbody.png' />
-                      {/* <!-- 트럭 몸통 --> */}
-                    </div>
-                  </div>
-                  <div className={styles.mars_buildings}></div>
-                  <div className={styles.mars_parallax}></div>
-                  <div className={styles.mars_parallax__two}></div>
-
-                  {/* <!-- 1 --> */}
-
-                  <div className={`${styles.mars_scene} ${styles.start}`}>
-                    <div className={styles.mars_scene__inner}>
-                      <div className={styles.intro}>
-                        <div className={styles.intro_small}>
-                          Move
-                        </div>
-                        <div className={styles.intro_to}>
-                          to
-                        </div>
-                        <div className={styles.intro_large}>
-                          <span>M</span>
-                          <span>a</span>
-                          <span>r</span>
-                          <span>s</span>
-                        </div>
-                        <div className={styles.intro_subheading}>
-                          " Your new home awaits"
-                        </div>
-                        <label for='index2'>
-                          Get started
-                          <img className={styles.rocket} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/rocket_copy.png' />
-                          {/* <!-- 로켓 --> */}
-                            <img className={styles.cloud} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/rcloud.png' />
-                            {/* <!-- 로켓발사 방귀--> */}
-                            </label>
-                          </div>
-                      </div>
-                    </div>
-
-                    {/* <!-- 2 --> */}
-
-                    <div className={`${styles.mars_scene} ${styles.development}`}>
-                      <div className={styles.mars_scene__inner}>
-                        <div className={styles.intro_development}>
-                          <div className={styles.intro_development__small}>
-                            Choose a
-                          </div>
-                          <div className={styles.intro_development__large}>
-                            <span>D</span>
-                            <span>e</span>
-                            <span>v</span>
-                            <span>e</span>
-                            <span>l</span>
-                            <span>o</span>
-                            <span>p</span>
-                            <span>m</span>
-                            <span>e</span>
-                            <span>n</span>
-                            <span>t</span>
-                          </div>
-                          <div className={styles.intro_development__mars}>
-                            <label for='index3'>
-                              <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/mars_big.png' />
-                              {/* <!-- 첫 화면에 있는 큰 화성--> */}
-                            </label>
-                          </div>
-                        </div>
-                        <div className={styles.intro_peeps}>
-                          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/peeps.png' />
-                          {/* <!--화성에 놀러간 4사람들--> */}
-                        </div>
-                      </div>
-                    </div>
-
-                    {/* <!-- 3 --> */}
-
-                    <div className={`${styles.mars_scene} ${styles.people}`}>
-                      <div className={styles.mars_scene__inner}>
-                        <div className={styles.intro_people}>
-                          <div className={styles.wrap}>
-                            <div className={styles.intro_people__small}>
-                              How many
-                            </div>
-                            <div className={styles.intro_people__large}>
-                              <span>P</span>
-                              <span>e</span>
-                              <span>o</span>
-                              <span>p</span>
-                              <span>l</span>
-                              <span>e</span>
-                            </div>
-                          </div>
-                          <label for='index4'>Solo</label>
-                          <label for='index4'>Couple</label>
-                          <label for='index4'>Family</label>
-                          <div className={styles.intro_people__select}>
-                            <img className={styles.fam1} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fam1.png' />
-                            {/* <!-- 아빠--> */}
-                              <img className={styles.fam2} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fam2.png' />
-                              {/* <!-- 아빠 엄마--> */}
-                                <img className={styles.fam3} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fam3.png' />
-                                {/* <!-- 아빠 엄마 아가--> */}
-                                  <img className={styles.fam4} src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/fam4.png' />
-                                  {/* <!-- 가방 --> */}
-                                  </div>
-                                </div>
-                              </div>
-                          </div>
-
-                          {/* <!-- 4 --> */}
-
-                          <div className={`${styles.mars_scene} ${styles.hab}`}>
-                            <div className={styles.mars_scene__inner}>
-                              <div className={styles.intro_hab}>
-                                <div className={styles.intro_hab__small}>
-                                  choose a
-                                </div>
-                                <div className={styles.intro_hab__large}>
-                                  <span>L</span>
-                                  <span>a</span>
-                                  <span>u</span>
-                                  <span>n</span>
-                                  <span>c</span>
-                                  <span>h</span>
-                                  <span></span>
-                                  <span>d</span>
-                                  <span>a</span>
-                                  <span>t</span>
-                                  <span>e</span>
-                                </div>
-                                <div className={styles.intro_hab__habs}>
-                                  <label for='index5'>January</label>
-                                  <label for='index5'>Febraury</label>
-                                  <label for='index5'>March</label>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-
-                          {/* <!-- 5 --> */}
-
-                          <div className={`${styles.mars_scene} ${styles.hab}`}>
-                            <div className={styles.mars_scene__inner}>
-                              <div className={styles.intro_hab}>
-                                <div className={styles.intro_hab__small}>
-                                  Booking complete
-                                </div>
-                                <div className={styles.intro_hab__large}>
-                                  <span>T</span>
-                                  <span>h</span>
-                                  <span>a</span>
-                                  <span>n</span>
-                                  <span>k</span>
-                                  <span>s</span>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={`${styles.mars_scene} ${styles.end}`}>
-                            <div className={styles.mars_scene__inner}>
-                              <div className={styles.intro_slots}>
-                                <div className={styles.intro_slots__small}>
-                                  thanks
-                                </div>
-                                <div className={styles.intro_slots__large}>
-                                  <span>w</span>
-                                  <span>e</span>
-                                  <span>w</span>
-                                  <span>t</span>
-                                </div>
-                                <div className={styles.intro_slots_slot}>
-                                  <label for='index6'></label>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={styles.mars_sidebar}>
-                            <h2>Cost</h2>
-                            <p>Development: -</p>
-                            <p>People: -</p>
-                            <p>Hab type: -</p>
-                            <p>Booking slot: -</p>
-                          </div>
-                        </div>
-                        </main>
-                        )
+      <div className={styles.bottom}></div>
+    </main>
+  )
 }
 
-                        export default Quiz
+export default Quiz
